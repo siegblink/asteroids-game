635c39b174868c5d13498a90a848ec09ffbc80d1
Sieg Balona <siegblink@gmail.com>
2025-09-30 17:31:36 +0800

    feat: add update and rotate methods in player class
    - Used update method in main.py

--- main.py
+++ main.py
@@ -9,27 +9,28 @@ from player import Player
 def main():
     pygame.init()
 
-    clock = pygame.time.Clock()
-    dt = 0
-
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
-        ms = clock.tick(60)
-        dt = ms / 1000
+        # Limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
